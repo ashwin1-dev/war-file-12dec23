c6c1bef52c1c8613023ce919a991dab96cd1ceef
ashwin1-dev <ashwin.devop12@gmail.com>
2023-12-12 07:19:15 +0000

    war file

---
+++ src/main/java/com/web/prj6/App.java
@@ -0,0 +1,15 @@
+package com.web.prj6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello India" );
+        System.out.println( "Hello Maharashtra" );
+    }
+}
